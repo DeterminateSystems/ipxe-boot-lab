9378aeb64a66b2a42ceb16f7d0e0968074e70941
Cole Helbling <cole.helbling@determinate.systems>
2021-09-24 09:15:40 -0700

    vm: support non-interactive mode

---
+++ vm/src/tty_handler/manual.rs
@@ -0,0 +1,46 @@
+use std::io::BufReader;
+use std::os::unix::net::UnixStream;
+
+use qapi::{qmp, Qmp, Stream};
+
+use super::QemuHandler;
+use crate::Result;
+
+#[derive(Debug)]
+pub struct Manual {
+    pub monitor: String,
+    pub serials: Vec<String>,
+}
+
+impl QemuHandler for Manual {
+    fn qemu_args(&self) -> Vec<String> {
+        let mut args = vec!["-chardev".to_string(), format!("pty,id={}", self.monitor)];
+
+        for serial in &self.serials {
+            args.extend(["-chardev".to_string(), format!("pty,id={}", serial)]);
+        }
+
+        args
+    }
+
+    fn setup(&self, qmp: &mut Qmp<Stream<BufReader<&UnixStream>, &UnixStream>>) -> Result<()> {
+        let chardevs = qmp.execute(&qmp::query_chardev {})?;
+
+        for dev in chardevs {
+            if !self.serials.iter().any(|n| dev.label == *n) && dev.label != self.monitor {
+                continue;
+            }
+
+            println!(
+                ": {} ; screen {} 115200",
+                dev.label,
+                dev.filename.trim_start_matches("pty:")
+            );
+        }
+
+        // give you time to run screen
+        std::thread::sleep_ms(5000);
+
+        Ok(())
+    }
+}
---
+++ vm/src/tty_handler/stdout.rs
@@ -0,0 +1,35 @@
+use std::io::BufReader;
+use std::os::unix::net::UnixStream;
+
+use qapi::{Qmp, Stream};
+
+use super::QemuHandler;
+use crate::Result;
+
+#[derive(Debug)]
+pub struct Stdout {
+    pub monitor: String,
+    pub serials: Vec<String>,
+}
+
+impl QemuHandler for Stdout {
+    fn qemu_args(&self) -> Vec<String> {
+        let mut args = vec![
+            "-chardev".to_string(),
+            format!("file,id={},path=/dev/stdout", self.monitor),
+        ];
+
+        for serial in &self.serials {
+            args.extend([
+                "-chardev".to_string(),
+                format!("file,id={},path=/dev/stdout", serial),
+            ]);
+        }
+
+        args
+    }
+
+    fn setup(&self, _qmp: &mut Qmp<Stream<BufReader<&UnixStream>, &UnixStream>>) -> Result<()> {
+        Ok(())
+    }
+}
